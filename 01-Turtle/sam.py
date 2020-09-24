cfbe7757249f77e53e08dab908a042fcbbbd98b8
Sam <sam0000@live.nl>
2020-09-24 10:43:21 +0200

    turtleopdrachten

---
+++ 01-Turtle/sam.py
@@ -0,0 +1,15 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+
+for x in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    
+turtle.done()
